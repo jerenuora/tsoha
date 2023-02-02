cd19bfd564d39d30a496122cd2dde898bce83d41
Jere Nuora <jere.nuora@gmail.com>
2023-02-02 19:13:32 +0200

    fly deploy finished

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from app import app
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.secret_key = getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
