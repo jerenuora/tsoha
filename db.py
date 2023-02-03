36ece9daf7ad40c0ae3c1db0ee340ef93ae14ee9
Jere Nuora <jere.nuora@gmail.com>
2023-02-03 20:14:51 +0200

    linting

--- db.py
+++ db.py
@@ -5,10 +5,10 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 
 if getenv("DEV") == "True":
-    
     app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-else: 
-    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv(
+        "DATABASE_URL").replace("://", "ql://", 1)
 
 app.secret_key = getenv("SECRET_KEY")
 
--- routes.py
+++ routes.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql import text
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 @app.route("/")
 def index():
     sql = "SELECT id, name FROM forums"
@@ -13,49 +14,51 @@ def index():
     forums = result.fetchall()
     return render_template("index.html", forums=forums)
 
+
 @app.route("/forum/<int:id>")
 def thread_view(id):
     sql = "SELECT id, title, forum_id FROM threads WHERE forum_id=:id"
-    result = db.session.execute(text(sql), {"id":id})
+    result = db.session.execute(text(sql), {"id": id})
     threads = result.fetchall()
     print(threads)
     return render_template("threads.html", threads=threads, forum_id=id)
+
 
 @app.route("/forum/thread/<int:id>")
 def messages_view(id):
     sql = """SELECT id, writer, message, date, thread_id
     FROM messages WHERE thread_id=:id"""
-    result = db.session.execute(text(sql), {"id":id})
+    result = db.session.execute(text(sql), {"id": id})
     messages = result.fetchall()
     return render_template("messages.html", messages=messages, thread_id=id)
+
 
 @app.route("/loginpage")
 def loginpage():
     return render_template("loginpage.html")
 
 
-@app.route("/login",methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
     sql = "SELECT id, password FROM users WHERE username=:username"
-    result = db.session.execute(text(sql), {"username":username})
-    user = result.fetchone()    
+    result = db.session.execute(text(sql), {"username": username})
+    user = result.fetchone()
 
     if not user:
-            flash("Väärä käyttäjänimi")
-            return redirect("/loginpage")
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):
-                session["username"] = username
-                return redirect("/")
-        else:
-            flash("Väärä salasana")
-            return redirect("/loginpage")
+        flash("Väärä käyttäjänimi")
+        return redirect("/loginpage")
 
- 
- 
+    hash_value = user.password
+    if not check_password_hash(hash_value, password):
+        flash("Väärä salasana")
+        return redirect("/loginpage")
+
+    session["username"] = username
+    return redirect("/")
+
+
 @app.route("/logout")
 def logout():
     del session["username"]
@@ -66,44 +69,51 @@ def logout():
 def signuppage():
     return render_template("signuppage.html")
 
-@app.route("/signup",methods=["POST"])
+
+@app.route("/signup", methods=["POST"])
 def signup():
     username = request.form["username"]
     password = request.form["password"]
     hash_value = generate_password_hash(password)
     sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-    db.session.execute(text(sql), {"username":username, "password":hash_value})
+    db.session.execute(
+        text(sql), {"username": username, "password": hash_value})
     db.session.commit()
 
     session["username"] = username
     return redirect("/")
 
-@app.route("/postmessage/<int:id>",methods=["POST"])
+
+@app.route("/postmessage/<int:id>", methods=["POST"])
 def postmessage(id):
     message = request.form["message"]
     writer = session["username"]
     thread_id = id
     sql = "INSERT INTO messages (writer, message, thread_id) VALUES (:writer, :message, :thread_id)"
 
-    db.session.execute(text(sql), {"writer":writer, "message":message, "thread_id":thread_id})
+    db.session.execute(
+        text(sql), {"writer": writer, "message": message, "thread_id": thread_id})
     db.session.commit()
     return redirect(request.referrer)
 
-@app.route("/postthread/<int:id>",methods=["POST"])
+
+@app.route("/postthread/<int:id>", methods=["POST"])
 def postthread(id):
     title = request.form["title"]
     message = request.form["message"]
     writer = session["username"]
     sql = "INSERT INTO threads (title, owner, forum_id) VALUES (:title, :owner, :forum_id)"
-    db.session.execute(text(sql), {"title":title, "owner":writer, "forum_id":id})
+    db.session.execute(
+        text(sql), {"title": title, "owner": writer, "forum_id": id})
     db.session.commit()
 
     sql = "SELECT MAX(id) FROM threads WHERE forum_id=:id"
-    result = db.session.execute(text(sql), {"id":id})
+    result = db.session.execute(text(sql), {"id": id})
     thread_id = result.fetchall()
 
     sql = "INSERT INTO messages (writer, message, thread_id) VALUES (:writer, :message, :thread_id)"
 
-    db.session.execute(text(sql), {"writer":writer, "message":message, "thread_id":thread_id[0][0]})
+    db.session.execute(
+        text(sql), {"writer": writer, "message": message, "thread_id": thread_id[0][0]})
     db.session.commit()
     return redirect(request.referrer)
