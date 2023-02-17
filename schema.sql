76b9a56f3af883e8f2f224414e36c08d8ad4f9cd
Jere Nuora <jere.nuora@gmail.com>
2023-02-17 18:06:12 +0200

    enable deletion in schema with cascading on delete

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE threads (
     id SERIAL PRIMARY KEY, 
     title TEXT, 
     owner TEXT,
-    forum_id INTEGER REFERENCES forums
+    forum_id INTEGER REFERENCES forums ON DELETE CASCADE
 );
 
 CREATE TABLE messages (
@@ -15,7 +15,7 @@ CREATE TABLE messages (
     writer TEXT NOT NULL,
     message TEXT NOT NULL,
     date DATE NOT NULL DEFAULT CURRENT_DATE,
-    thread_id INTEGER REFERENCES threads
+    thread_id INTEGER REFERENCES threads ON DELETE CASCADE
 );
 
 CREATE TABLE users (
